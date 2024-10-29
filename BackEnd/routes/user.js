a46728d78962abe57765a10d3729d23362f9ef68
ashishjha20 <ashish.20233087@mnnit.ac.in>
2024-10-29 13:10:05 +0530

    Update user.js

--- BackEnd/routes/user.js
+++ BackEnd/routes/user.js
@@ -4,11 +4,14 @@ const registerUser = require('../Controllers/registerUser');
 const loginUser=require('../Controllers/loginUser');
 const {getDashboardData}=require('../Controllers/getDashboardData');
 const verifyOtp = require('../Controllers/verifyOtp');
-
+const {addAssignment} =require('../Controllers/addAssignment')
+const {getAssignmentsByEmail} =require('../Controllers/getAssignmentsByEmail')
 
 router.post('/register', registerUser);
 router.post('/login',loginUser);
 router.get('/dashboard/:email',getDashboardData);
 router.post('/verify-otp',verifyOtp);
+router.post('/assignment',addAssignment )
+router.get('/assignment/:email',getAssignmentsByEmail)
 
 module.exports = router;
