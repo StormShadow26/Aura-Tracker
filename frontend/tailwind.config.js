1f9114d4410541bfd0be388ded40e7ad99620bef
StormShadow26 <tanishqgupta2710@gmail.com>
2024-10-30 16:10:11 +0530

    Finalised HeroPage,All Basic chize done

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,10 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        fadeIn: {
+          "0%": { opacity: 0, transform: "scale(0.95)" },
+          "100%": { opacity: 1, transform: "scale(1)" },
+        },
+      },
+      animation: {
+        "fade-in": "fadeIn 0.3s ease-out forwards",
+      },
+    },
   },
   plugins: [],
 };
