685dfa2645b791fdfc421020888611b3dda007d1
ashishjha20 <ashish.20233087@mnnit.ac.in>
2024-11-05 14:49:50 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -20,11 +20,8 @@ import QuizPage from './components/QuizPage';
 import Room from  './components/Room';
 import VideoCall from './components/VideoCall'
 import Mentors from './components/Mentors'
-<<<<<<< Updated upstream
 import ChallengeForm from './components/ChallengeForm';
-=======
 import CodeEditor from './components/CodeEditor';
->>>>>>> Stashed changes
 
 
 function App() {
@@ -63,12 +60,9 @@ function App() {
         <Route path="/quiz" element={<QuizPage/>} />
         <Route path="/room" element={<Room/>} />
         <Route path="/room/:roomId" element={<VideoCall/>} />
-<<<<<<< Updated upstream
-=======
         <Route path="/mentors" element={<Mentors/>} />
         <Route path="/coder" element={<CodeEditor/>} />
 
->>>>>>> Stashed changes
 
         
 
